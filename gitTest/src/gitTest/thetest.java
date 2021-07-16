43cb5de7e85115d1246d28d4bbfe939738817b95
Dys <dysismedia@gmail.com>
2021-07-16 10:45:17 +0200

    Primero, todos archivos

---
+++ gitTest/src/gitTest/thetest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gitTest;
+
+/**
+ * @author Muri
+ *
+ */
+public class thetest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
